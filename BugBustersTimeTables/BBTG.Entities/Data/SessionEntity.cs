f0196918154a98e619a2683ad6f11a49a40dba3f
arojayathilake <acjayathilake@gmail.com>
2020-09-13 20:54:48 +0530

    session initial commit

---
+++ BugBustersTimeTables/BBTG.Entities/Data/SessionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTG.Entities.Data
+{
+    public class SessionEntity
+    {
+        public LecturerEntity Lecturer { get; set; }
+        public string SubjectName { get; set; }
+        public string SubjectCode { get; set; }
+        public string Tag { get; set; }
+    }
+}
