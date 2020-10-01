3101829cf780e308864f3e4024d8950fa42b0e7d
arojayathilake <acjayathilake@gmail.com>
2020-10-01 19:24:30 +0530

    time slots test

--- BugBustersTimeTables/BBTG.Common/Config/AppData.cs
+++ BugBustersTimeTables/BBTG.Common/Config/AppData.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BBTG.Entities.Data;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BBTG.Common.Config
@@ -7,5 +10,40 @@ namespace BBTG.Common.Config
     public class AppData
     {
         public static string ConnectionString { get; set; }
+
+        public static List<TimeSlotEntity> _timeSlotEntities { get; private set; }
+
+
+        public AppData()
+        {
+            TimeSlotEntity x = new TimeSlotEntity();
+
+
+            string[] days = new string[] { "Mon", "Tue", "Wed", "Thu", "Fri" };
+            //For day : - 9 ? 
+            // # of days : - 5 ? 
+            // TOT = 9*5 = 45
+            _timeSlotEntities = new List<TimeSlotEntity>();
+            int startTime = 8;
+            int currentDate = 0;
+            int endTime = 17;
+            for (int i = 1; i < 46; i++)
+            {
+                if (startTime == endTime) 
+                {
+                    startTime = 8;
+                    currentDate++;
+                }
+ 
+                _timeSlotEntities.Add(
+                    new TimeSlotEntity { ID = i, TImeSlot = $"{startTime}-{++startTime}",Date=days[currentDate]}
+                    ); 
+            }
+
+       
+         }
+
+        
+            
     }
 }
--- BugBustersTimeTables/Time_Table_Generator/ViewModel/SessionViewModel.cs
+++ BugBustersTimeTables/Time_Table_Generator/ViewModel/SessionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using BBTG.DataAccess;
+﻿using BBTG.Common.Config;
+using BBTG.DataAccess;
+using BBTG.Entities;
 using BBTG.Entities.Data;
 using System;
 using System.Collections.Generic;
@@ -11,15 +13,114 @@ namespace Time_Table_Generator.ViewModel
     internal class SessionViewModel
     {
         SessionData _sessionData;
+        List<SessionEntity> CurrentSessionDataList;
+        List<LecturerEntity> CurrentlecturersList;
+
+
+        //Support ViewModel
+        LecturerViewModel _lecturerViewModel;
+        
 
         public SessionViewModel()
         {
             _sessionData = new SessionData();
+            _lecturerViewModel = new LecturerViewModel();
         }
 
         public List<SessionEntity> LoadSessionData()
         {
-            return _sessionData.LoadData();
+            CurrentlecturersList = _lecturerViewModel.LoadLecturerData();
+            CurrentSessionDataList = _sessionData.LoadData();
+            CurrentSessionDataList = SetLecturers(CurrentSessionDataList);
+            __Test();
+            return CurrentSessionDataList;
+        }
+
+        public List<SessionEntity> SetLecturers(List<SessionEntity> sessions)
+        {
+            return sessions.Select(e => { e.SessionLectures = LecturerEntities(e.LecturerIdsArr); return e; }).ToList();
+        }
+
+        /*
+         CONDITIONS : - 
+                1. Load Data According to the Score
+         
+         */
+
+        public void __Test() 
+        {
+            // loop through and get the lowest ranked lecturer and order sessions by him
+            CurrentSessionDataList.OrderBy(e => e.SessionLectures.Select(ee => ee.Rank).Min()).ToList();
+            // load timeslots
+            var x = AppData._timeSlotEntities;
+
+            //assign sessions to each time slot
+            x.Select(e => { e.SessionEntity = SessionEntities(x); return e; }).ToList();
+        }
+
+        public List<Session> GetSessionList(List<SessionEntity> sessions)
+        {
+            List<Session> sessionList = new List<Session>();
+            sessions = SetLecturers(sessions);
+            foreach (SessionEntity s in sessions)
+            {
+                List<string> lecNames = new List<string>();
+                foreach (LecturerEntity l in s.SessionLectures)
+                {
+                    lecNames.Add(l.Name);
+                }
+                string[] slctdLecsNames = lecNames.ToArray();
+                
+                sessionList.Add(new Session
+                {
+                    Lecturers = string.Join(",", slctdLecsNames),
+                    SubjectName = s.SubjectName,
+                    SubjectCode = s.SubjectCode,
+                    Tag = s.Tag,
+                    GroupId = s.GroupId,
+                    Count = s.Count,
+                    Duration = s.Duration
+                });
+            }
+            return sessionList;
+        }
+
+        private List<LecturerEntity> LecturerEntities(int[] vs) 
+        {
+            List<LecturerEntity> d = new List<LecturerEntity>();
+            for (int i = 0; i < vs.Length; i++)
+                d.Add(CurrentlecturersList.Find(e => e.EmployeeId == vs[i]));
+            
+            return d;
+        }
+
+
+
+        private List<SessionEntity> SessionEntities(List<TimeSlotEntity> currentList)
+        {
+
+
+            //foreach (var item in currentList)
+            //{
+            //    if (item.SessionEntity.Any(e => CurrentSessionDataList.Any(eee => eee.SessionId == e.SessionId)))
+            //        return null;
+            //    else
+            //        return CurrentSessionDataList;
+            //}
+            //return null;
+
+            try
+            {
+                // check if any sessions that are alraedy assigned to timeslots
+                if (!currentList.Any(e => e.SessionEntity.Any(ee => CurrentSessionDataList.Any(eee => eee.SessionId == ee.SessionId))))
+                    return CurrentSessionDataList;
+                else
+                    return new List<SessionEntity>();
+            }
+            catch (Exception ex)
+            {
+                return CurrentSessionDataList;
+            }
         }
 
         public List<SessionEntity> LoadSessionDataBySubject(string subjectCode)
